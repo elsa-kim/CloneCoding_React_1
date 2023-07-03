aa7b614995544c9358df62b681d6d5bf4133fea6
elsa-kim <ksh96611@naver.com>
2023-07-03 21:54:22 +0900

    sidebar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDuWVPpECXDqol9zrqpFa8pGDDhG8FN17o",
+  authDomain: "whats-app-clone-360ad.firebaseapp.com",
+  projectId: "whats-app-clone-360ad",
+  storageBucket: "whats-app-clone-360ad.appspot.com",
+  messagingSenderId: "191369123760",
+  appId: "1:191369123760:web:5b7b92fba8133a6788a875",
+  measurementId: "G-YK9BK19P15",
+};
