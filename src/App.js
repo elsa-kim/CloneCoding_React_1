12d997da16f540c74322936b847724913212db49
elsa-kim <ksh96611@naver.com>
2023-07-10 21:37:37 +0900

    login page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Login from "./Login";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
-      <div className="app__body">
-        <BrowserRouter>
-          <Routes>
-            {/* <Route path="/app" element={<Sidebar />} />
-            <Route path="/app" element={<Chat />} /> */}
-            <Route path="/" element={<h1>Home Screen</h1>} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <BrowserRouter>
+            <Sidebar />
+            <Routes>
+              <Route path="/rooms/:roomId" element={<Chat />} />
+              <Route path="/" element={<Chat />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      )}
     </div>
   );
 }
--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,27 @@
 import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from "@mui/icons-material";
 import { Avatar, IconButton } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./Chat.css";
+import db from "./firebase";
 
 function Chat() {
   const [input, setInput] = useState("");
   const [seed, setSeed] = useState("");
+  const { roomId } = useParams();
+  const [roomName, setRoomName] = useState("");
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomName(snapshot.data().name));
+    }
+  }, [roomId]);
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
-  }, []);
+  }, [roomId]);
 
   const sendMessage = (e) => {
     e.preventDefault();
@@ -22,7 +34,7 @@ function Chat() {
       <div className="chat__header">
         <Avatar src={`http://avatars.dicebear.com/api/human/${seed}.svg`} />
         <div className="chat__headerInfo">
-          <h3>Room name</h3>
+          <h3>{roomName}</h3>
           <p>Last seen at...</p>
         </div>
         <div className="chat__headerRight">
